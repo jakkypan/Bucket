489d3dba20cd54fcf8cedd4b67ad5fd5214f8194
jakkypan <hcpan1119@163.com>
2016-03-07 21:51:27 +0800

    percentfragment

---
+++ percent/src/main/java/com/pan/learn/percent/PercentRelativeLayoutActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2016 Baidu, Inc. All Rights Reserved.
+ */
+package com.pan.learn.percent;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by panhongchao on 16/3/7.
+ */
+public class PercentRelativeLayoutActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.view5);
+    }
+}
