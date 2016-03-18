6a77c2d09207da1bfcb5b5c9d3ad4948afe743ee
jakkypan <hcpan1119@163.com>
2016-03-18 16:33:00 +0800

    trival

--- percent/src/main/java/com/pan/learn/percent/PercentRelativeLayoutActivity.java
+++ percent/src/main/java/com/pan/learn/percent/PercentRelativeLayoutActivity.java
@@ -5,6 +5,7 @@ package com.pan.learn.percent;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * Created by panhongchao on 16/3/7.
@@ -13,7 +14,7 @@ public class PercentRelativeLayoutActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+//        Toast.makeText(this, System.getProperty("java.vm.version"), Toast.LENGTH_LONG).show();
         setContentView(R.layout.view5);
     }
 }
